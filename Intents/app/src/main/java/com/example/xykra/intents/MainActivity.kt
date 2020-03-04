5c7a5c18cb71118e8dea5add4e54ea1c3756796e
JavierYucraZ <xavwlr20@gmail.com>
2020-03-03 20:03:55 -0400

    imagenes

--- Intents/app/src/main/java/com/example/xykra/intents/MainActivity.kt
+++ Intents/app/src/main/java/com/example/xykra/intents/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.xykra.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnIrSegunda.setOnClickListener{
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnIrTercera.setOnClickListener{
+            val intent = Intent(this, TerceraActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
