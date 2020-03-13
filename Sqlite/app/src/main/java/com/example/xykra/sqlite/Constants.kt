4dfcc85a8e7b5a93fa07f4ecc7b772933debb218
JavierYucraZ <xavwlr20@gmail.com>
2020-03-13 15:19:33 -0400

    SQLite

---
+++ Sqlite/app/src/main/java/com/example/xykra/sqlite/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.xykra.sqlite
+
+object Constants {
+
+    val TABLA_PERSONA = "persona"
+    val ID = "_id"
+    val NOMBRE = "nombre"
+    val TELEFONO = "telefono"
+    val CORREO = "correo"
+    val GENERO = "genero"
+
+}
